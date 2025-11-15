c4c50caa996991624da377324affca65e62e5386
Giorgi Nikabadze <iceerpgeorgia@gmail.com>
2025-11-15 13:29:26 +0400

    fix: correct type casting in trigger SQL scripts

--- scripts/step1-create-function.sql
+++ scripts/step1-create-function.sql
@@ -1,19 +1,16 @@
--- Step 1: Create the function
 CREATE OR REPLACE FUNCTION public.counteragents_populate_names()
 RETURNS trigger
 LANGUAGE plpgsql
 AS $$
 BEGIN
-  -- Populate country from country_uuid
   IF NEW.country_uuid IS NOT NULL THEN
     SELECT c.name_ka INTO NEW.country
     FROM public.countries c
-    WHERE c.country_uuid::text = NEW.country_uuid;
+    WHERE c.country_uuid = NEW.country_uuid::uuid;
   ELSE
     NEW.country := NULL;
   END IF;
 
-  -- Populate entity_type from entity_type_uuid
   IF NEW.entity_type_uuid IS NOT NULL THEN
     SELECT et.name_ka INTO NEW.entity_type
     FROM public.entity_types et
