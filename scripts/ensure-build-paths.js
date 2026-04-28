31e5d5fe29b14865c2f2fb4e84b729c8d3186e72
Giorgi Nikabadze <iceerpgeorgia@gmail.com>
2026-04-28 17:44:28 +0400

    fix(deploy): revert prebuild workaround, clean vercelignore [skip ci]

--- scripts/ensure-build-paths.js
+++
@@ -1,8 +0,0 @@
-const fs = require('fs');
-const path = require('path');
-
-const deployLogPath = path.join(process.cwd(), '_deploy-log');
-
-if (!fs.existsSync(deployLogPath)) {
-  fs.mkdirSync(deployLogPath, { recursive: true });
-}
